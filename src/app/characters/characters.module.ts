702214909508088c22d245896d516f07bbbb5492
diegoregino <diegoregino1994@gmail.com>
2019-09-29 03:20:34 -0500

    select and character title markup

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CharactersRoutingModule } from './characters-routing.module';
 import { CharactersListComponent } from './characters-list/characters-list.component';
 import { FavoriteComicsComponent } from './favorite-comics/favorite-comics.component';
+import { SortByComponent } from './sort-by/sort-by.component';
 
 
 @NgModule({
-  declarations: [CharactersListComponent, FavoriteComicsComponent],
+  declarations: [CharactersListComponent, FavoriteComicsComponent, SortByComponent],
   imports: [
     CommonModule,
     CharactersRoutingModule
